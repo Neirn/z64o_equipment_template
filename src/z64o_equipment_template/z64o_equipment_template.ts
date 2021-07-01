eaa1c152ea6e41ec097613b4ca83da179366192f
Neirn <neirny@gmail.com>
2021-07-01 11:53:22 -0700

    Automatically embed zobj name if one does not already exist

--- src/z64o_equipment_template/z64o_equipment_template.ts
+++ src/z64o_equipment_template/z64o_equipment_template.ts
@@ -1,7 +1,7 @@
 import { IPlugin, IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
 import { bus } from 'modloader64_api/EventHandler';
 import { OotOnlineEvents, Z64Online_EquipmentPak } from './Z64API/OotoAPI';
-import path from 'path';
+import { resolve, basename, join } from 'path';
 import { IOOTCore } from 'modloader64_api/OOT/OOTAPI';
 import { InjectCore } from 'modloader64_api/CoreInjection';
 import { MMOnlineEvents } from './Z64API/MMOAPI';
@@ -20,6 +20,8 @@ class zzdata {
   MajorasMask!: MM_ZZ;
 }
 
+type Z64OnlineEvents = OotOnlineEvents | MMOnlineEvents;
+
 class zzplayas implements IPlugin {
   ModLoader!: IModLoaderAPI;
   pluginName?: string | undefined;
@@ -31,26 +33,12 @@ class zzplayas implements IPlugin {
     let zz: zzdata = (this as any)['metadata']['zzequipment'];
     let OOT = () => {
       zz.OcarinaOfTime.equipment.forEach(zobj => {
-        if (zobj !== '') {
-          let data = readFileSync(path.resolve(path.join(__dirname, zobj)));
-          let name = this.ModLoader.utils.hashBuffer(data);
-          bus.emit(
-            OotOnlineEvents.LOAD_EQUIPMENT_PAK,
-            new Z64Online_EquipmentPak(name, data)
-          );
-        }
+        this.processZobj(zobj, OotOnlineEvents.LOAD_EQUIPMENT_PAK);
       });
     };
     let MM = () => {
       zz.MajorasMask.equipment.forEach(zobj => {
-        if (zobj !== '') {
-          let data = readFileSync(path.resolve(path.join(__dirname, zobj)));
-          let name = this.ModLoader.utils.hashBuffer(data);
-          bus.emit(
-            MMOnlineEvents.LOAD_EQUIPMENT_PAK,
-            new Z64Online_EquipmentPak(name, data)
-          );
-        }
+        this.processZobj(zobj, MMOnlineEvents.LOAD_EQUIPMENT_PAK);
       });
     };
 
@@ -59,6 +47,47 @@ class zzplayas implements IPlugin {
   }
   postinit(): void { }
   onTick(): void { }
+
+
+  processZobj(zobj: string, event: Z64OnlineEvents) {
+    if (zobj !== '') {
+      let data = readFileSync(resolve(join(__dirname, zobj)));
+      let name = this.ModLoader.utils.hashBuffer(data);
+
+      let nameOffset = name.indexOf("EQUIPMENTNAME");
+
+      if (nameOffset == -1) {
+        throw new Error('INVALID EQUIPMENT MANIFEST: ' + zobj);
+      }
+
+      // set name to filename if zobj doesn't have name embedded
+      if (data.slice(nameOffset + 0x20, nameOffset + 0x2C).toString('utf8') === "EQUIPMENTCAT") {
+        if (data.slice(nameOffset + 0x10, nameOffset + 0x20).toString('hex') === "00000000000000000000000000000000") {
+          let defaultName = basename(zobj, '.zobj');
+          if (defaultName.length >= 32) {
+            defaultName = defaultName.substring(0, 32);
+          }
+
+          let nameBuf = Buffer.from(defaultName);
+
+          let catOffset = data.indexOf("EQUIPMENTCAT");
+
+          if (catOffset == -1) {
+            throw new Error('INVALID EQUIPMENT MANIFEST: ' + zobj);
+          }
+
+          data = Buffer.concat([data.slice(0, nameOffset + 0x10), nameBuf, Buffer.alloc(nameBuf.length % 0x10, 0), data.slice(catOffset)]);
+        }
+      }
+
+      bus.emit(
+        event,
+        new Z64Online_EquipmentPak(name, data)
+      );
+    }
+  }
+
+
 }
 
 module.exports = zzplayas;
